684222fe3f43b1fe81b4f6409c7167ee43671b42
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2022-11-19 19:24:28 +0530

    js change

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,38 +1,36 @@
-var quizForm = document.querySelector(".quiz-form");
-var button = document.querySelector("#button");
-var displayText = document.querySelector("#display-text");
-var headingText = document.querySelector("#heading");
+var quizForm = document.querySelector('.quiz-form');
+var button = document.querySelector('#button');
+var displayText = document.querySelector('#display-text');
+var headingText = document.querySelector('#heading');
 
-headingText.style.display = "none";
+headingText.style.display = 'none';
 
 var correctAnswers = [
-    "90°",
-    "right angled",
-    "one right angle",
-    "12, 16, 20",
-    "Equilateral triangle",
-    "100°",
-    "30°",
-    "a + b + c",
-    "no",
-    "45°",
+  '90°',
+  'right angled',
+  'one right angle',
+  '12, 16, 20',
+  'Equilateral triangle',
+  '100°',
+  '30°',
+  'a + b + c',
+  'no',
+  '45°',
 ];
 
 function calculateScore() {
-
-    var data = new FormData(quizForm);
-    let score = 0;
-    let index = 0;
-        for (let value of data.values()) {
-            if (value === correctAnswers[index]) {
-                score++;
-            }
-            console.log(value);
-            index++;
-        }
-        headingText.style.display = "block";
-        displayText.innerText = score;
-    
+  var data = new FormData(quizForm);
+  let score = 0;
+  let index = 0;
+  for (let value of data.values()) {
+    if (value === correctAnswers[index]) {
+      score++;
+    }
+    console.log(value);
+    index++;
+  }
+  headingText.style.display = 'block';
+  displayText.innerText = `${score} out of ${index}`;
 }
 
-button.addEventListener("click", calculateScore);
+button.addEventListener('click', calculateScore);
